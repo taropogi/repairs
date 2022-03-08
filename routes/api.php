6cd610b93380512a8deb49e4c02d228b8a6e12a8
Richard Bernisca <taropogi_123@yahoo.com>
2022-03-08 09:26:22 +0800

    user removed /

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\RegisterController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('user', function (Request $request) {
     return $request->user();
 });
 
