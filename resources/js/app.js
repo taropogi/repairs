fa6d00ab265ab3c1e3829a11032a38e30920f665
Richard Bernisca <taropogi_123@yahoo.com>
2022-10-31 01:17:39 +0800

    add index for every project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,4 @@
 require("./bootstrap");
 
-import { createApp } from "vue";
-
-// For CPO program
-import App from "./apps/cpo/App.vue";
-import router from "./apps/cpo/router.js";
-import store from "./apps/cpo/store.js";
-const app = createApp(App);
-app.use(router);
-app.use(store);
-
-// For testing / experiment
-// import App from "./apps/test/App.vue";
-// const app = createApp(App);
-
-app.mount("#app");
+require("./apps/cpo/Index");
+//require("./apps/test/Index");
---
+++ resources/js/apps/cpo/Index.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+
+// For CPO program
+import App from "./App.vue";
+import router from "./router.js";
+import store from "./store.js";
+const app = createApp(App);
+app.use(router);
+app.use(store);
+
+app.mount("#app");
