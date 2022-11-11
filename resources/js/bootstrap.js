ca3af3deebb76310417be09ed5ebe025849dba03
Richard Bernisca <taropogi_123@yahoo.com>
2022-11-11 13:54:38 +0800

    add accep header axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -13,6 +13,7 @@ try {
 window.axios = require("axios");
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.headers.common["Accept"] = "application/json";
 window.axios.defaults.withCredentials = true;
 
 /**
