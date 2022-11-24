1e9fb0a2185d1adcb26bb4df3947964fad45d150
Richard Bernisca <taropogi_123@yahoo.com>
2022-11-25 01:50:32 +0800

    fix login

--- resources/js/apps/cpo/components/pages/loginPage.vue
+++ resources/js/apps/cpo/components/pages/loginPage.vue
@@ -6,131 +6,108 @@
         }"
     >
         <div class="row justify-content-center">
-            <div class="col-md-4 mt-5">
+            <div class="col-md-4">
                 <div class="card login-container text-white">
-                    <div class="card-header">Login</div>
-
-                    <div class="card-body p-5">
-                        <form
-                            method="POST"
-                            action="#"
-                            @submit.prevent="submitLoginForm"
-                        >
-                            <div class="row mb-3">
-                                <label
-                                    for="username"
-                                    class="col-md-4 col-form-label text-md-end"
-                                    >Username</label
-                                >
-
-                                <div class="col-md-6">
-                                    <input
-                                        id="username"
-                                        type="text"
-                                        class="form-control"
-                                        :class="inputValidClasses"
-                                        name="username"
-                                        v-model="loginFormData.username"
-                                        required
-                                        autocomplete="username"
-                                        autofocus
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label
-                                    for="password"
-                                    class="col-md-4 col-form-label text-md-end"
-                                    >Password</label
-                                >
-
-                                <div class="col-md-6">
-                                    <input
-                                        id="password"
-                                        type="password"
-                                        class="form-control"
-                                        :class="inputValidClasses"
-                                        name="password"
-                                        v-model="loginFormData.password"
-                                        required
-                                        autocomplete="current-password"
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="row m-0" v-if="showError">
-                                <div class="col-md-6 offset-md-4">
-                                    <div
-                                        class="alert alert-danger d-flex align-items-center p-1"
-                                        role="alert"
-                                    >
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="24"
-                                            height="24"
-                                            fill="currentColor"
-                                            class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2"
-                                            viewBox="0 0 16 16"
-                                            role="img"
-                                            aria-label="Warning:"
-                                        >
-                                            <path
-                                                d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                                            />
-                                        </svg>
-                                        <div>Invalid login</div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input
-                                            class="form-check-input"
-                                            type="checkbox"
-                                            name="remember"
-                                            id="remember"
-                                        />
-
-                                        <label
-                                            class="form-check-label"
-                                            for="remember"
-                                        >
-                                            Remember me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button
-                                        v-if="!isValidating"
-                                        type="submit"
-                                        class="btn btn-primary"
-                                    >
-                                        Login
-                                    </button>
-
-                                    <button
-                                        v-else
-                                        class="btn btn-primary"
-                                        type="button"
-                                        disabled
-                                    >
-                                        <span
-                                            class="spinner-border spinner-border-sm"
-                                            role="status"
-                                            aria-hidden="true"
-                                        ></span>
-                                        Loading...
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                    <div class="text-center p-5 pb-0">
+                        <h1>LOGIN</h1>
                     </div>
+
+                    <form
+                        @submit.prevent="submitLoginForm"
+                        class="p-5 pt-1 m-5 mt-0"
+                    >
+                        <div class="mb-3">
+                            <label for="username" class="form-label"
+                                >Username</label
+                            >
+                            <input
+                                id="username"
+                                type="text"
+                                class="form-control"
+                                :class="inputValidClasses"
+                                name="username"
+                                v-model="loginFormData.username"
+                                required
+                                autocomplete="username"
+                                autofocus
+                            />
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label"
+                                >Password</label
+                            >
+                            <input
+                                id="password"
+                                type="password"
+                                class="form-control"
+                                :class="inputValidClasses"
+                                name="password"
+                                v-model="loginFormData.password"
+                                required
+                                autocomplete="current-password"
+                            />
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input
+                                class="form-check-input"
+                                type="checkbox"
+                                name="remember"
+                                id="remember"
+                            />
+                            <label class="form-check-label" for="remember"
+                                >Remember me</label
+                            >
+                        </div>
+
+                        <div class="row m-0" v-if="showError">
+                            <div class="col-md-12">
+                                <div
+                                    class="alert alert-danger d-flex align-items-center p-1"
+                                    role="alert"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        fill="currentColor"
+                                        class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2"
+                                        viewBox="0 0 16 16"
+                                        role="img"
+                                        aria-label="Warning:"
+                                    >
+                                        <path
+                                            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+                                        />
+                                    </svg>
+                                    <div>Invalid login</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-grid gap-2">
+                            <button
+                                v-if="!isValidating"
+                                type="submit"
+                                class="btn btn-primary"
+                            >
+                                Login
+                            </button>
+
+                            <button
+                                v-else
+                                class="btn btn-primary"
+                                type="button"
+                                disabled
+                            >
+                                <span
+                                    class="spinner-border spinner-border-sm"
+                                    role="status"
+                                    aria-hidden="true"
+                                ></span>
+                                Loading...
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -220,5 +197,7 @@ export default {
 .login-container {
     background-color: hsl(200, 100%, 6%, 0.5);
     box-shadow: 0 0 15px 0 hsl(200, 100%, 6%, 0.5);
+
+    top: 30%;
 }
 </style>
