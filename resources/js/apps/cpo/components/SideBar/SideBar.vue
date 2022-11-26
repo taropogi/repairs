59353a4bece6fa9a91fbd64bee893c25983473b1
Richard Bernisca <taropogi_123@yahoo.com>
2022-11-27 02:58:35 +0800

    test again

--- resources/js/apps/cpo/components/SideBar/SideBar.vue
+++ resources/js/apps/cpo/components/SideBar/SideBar.vue
@@ -36,6 +36,12 @@
 
         <h1 class="text-white">Test here</h1>
         <h1 class="text-white">Test here</h1>
+        <h1 class="text-white">Test here</h1>
+        <h1 class="text-white">Test here</h1>
+        <h1 class="text-white">Test here</h1>
+        <h1 class="text-white">Test here</h1>
+        <h1 class="text-white">Test here</h1>
+        <h1 class="text-white">Test here</h1>
 
         <div v-if="userIsAdmin">
             <hr class="text-white" />
