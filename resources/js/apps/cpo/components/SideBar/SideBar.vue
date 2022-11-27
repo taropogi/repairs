5522b97afaf04ae0e2c62baf0729315b183cc2ab
Richard Bernisca <taropogi_123@yahoo.com>
2022-11-27 22:59:44 +0800

    side

--- resources/js/apps/cpo/components/SideBar/SideBar.vue
+++ resources/js/apps/cpo/components/SideBar/SideBar.vue
@@ -33,7 +33,7 @@
                 >
             </li>
         </ul>
-
+        <h1 class="text-white">test here</h1>
         <div v-if="userIsAdmin">
             <hr class="text-white" />
             <admin-links-user></admin-links-user>
