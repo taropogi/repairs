5b7e520c8a6cdb7da0f215f70d91c4080c0a344b
Richard Bernisca <taropogi_123@yahoo.com>
2024-11-12 15:15:48 +0800

    update headerLines

--- resources/js/apps/cpo/components/EditCpo/editCpo.vue
+++ resources/js/apps/cpo/components/EditCpo/editCpo.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="p-2">
-        <form-header @searched-header-row="setHeaderRow" :id="id"></form-header>
-        <header-lines
-            class="mt-2"
-            v-if="headerRow"
-            :header-id="id"
-            :header-is-locked="headerRow.locked"
-        ></header-lines>
-    </div>
+  <div class="p-2">
+    <form-header @searched-header-row="setHeaderRow" :id="id"></form-header>
+    <header-lines
+      class="mt-2"
+      v-if="headerRow"
+      :header-id="id"
+      :header-is-locked="headerRow.locked"
+    ></header-lines>
+  </div>
 </template>
 
 <script>
@@ -16,68 +16,68 @@ import FormHeader from "./FormHeader.vue";
 import { mapActions } from "vuex";
 
 export default {
-    components: {
-        FormHeader,
-        headerLines,
-    },
-    props: ["id"],
+  components: {
+    FormHeader,
+    headerLines,
+  },
+  props: ["id"],
 
-    data() {
-        return {
-            headerRow: null,
+  data() {
+    return {
+      headerRow: null,
 
-            isUpdatedAllLinesSuccess: false,
-        };
+      isUpdatedAllLinesSuccess: false,
+    };
+  },
+  methods: {
+    ...mapActions(["setActiveNav"]),
+    setHeaderRow(headerRow) {
+      this.headerRow = headerRow;
     },
-    methods: {
-        ...mapActions(["setActiveNav"]),
-        setHeaderRow(headerRow) {
-            this.headerRow = headerRow;
-        },
-    },
-    beforeCreate() {
-        this.$store.commit("setMainPageTitleHeader", "CPO - Edit");
-        // this.$store.commit("setActiveNav", {
-        //     nav: "edit-cpo",
-        // });
-    },
-    mounted() {
-        //  console.log("mounted");
-        // this.getCpoHeaderRow();
-        this.setActiveNav({
-            nav: "edit-cpo",
-        });
-    },
-    updated() {
-        //   console.log("new value: " + this.lineDetails.length);
-    },
+  },
+  beforeCreate() {
+    this.$store.commit("setMainPageTitleHeader", "CPO - Edit");
+    // this.$store.commit("setActiveNav", {
+    //     nav: "edit-cpo",
+    // });
+  },
+  mounted() {
+    //  console.log("mounted");
+    // this.getCpoHeaderRow();
+    this.setActiveNav({
+      nav: "edit-cpo",
+    });
+  },
+  updated() {
+    //   console.log("new value: " + this.lineDetails.length);
+  },
 };
 </script>
 
 <style scoped>
 .updated-header-enter-from {
-    opacity: 0;
-    transform: translateY(-30px);
+  opacity: 0;
+  transform: translateY(-30px);
 }
 
 .updated-header-enter-active {
-    transition: all 0.3s ease-out;
+  transition: all 0.3s ease-out;
 }
 .updated-header-enter-to {
-    opacity: 1;
-    transform: translateY(0);
+  opacity: 1;
+  transform: translateY(0);
 }
 
 .updated-header-leave-from {
-    opacity: 1;
-    transform: translateY(0);
+  opacity: 1;
+  transform: translateY(0);
 }
 
 .updated-header-leave-active {
-    transition: all 0.3s ease-in;
+  transition: all 0.3s ease-in;
 }
 .updated-header-leave-to {
-    opacity: 0;
-    transform: translateY(-30px);
+  opacity: 0;
+  transform: translateY(-30px);
 }
 </style>
--- resources/js/apps/cpo/components/EditCpo/headerLines.vue
+++ resources/js/apps/cpo/components/EditCpo/headerLines.vue
@@ -1,142 +1,143 @@
 <template>
-    <div>
-        <table class="table table-sm">
-            <thead class="table-warning">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">HCopy</th>
-                    <th scope="col">Qty Returned</th>
-                    <th scope="col">Unit</th>
-                    <th scope="col">Qty Inspect</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Good Condition</th>
-                    <th scope="col">Minor Repair/Clean</th>
-                    <th scope="col">Repair/Parts Needed</th>
-                    <th scope="col">Damaged</th>
-                    <th scope="col">Comments</th>
-                    <th scope="col">Doc #</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <header-line
-                    v-for="(line, index) in lines"
-                    :key="line.id"
-                    :line-number="index + 1"
-                    :line-details="line"
-                    :header-is-locked="headerIsLocked"
-                    @delete-line="getCpoLines"
-                ></header-line>
-            </tbody>
-        </table>
+  <div>
+    <table class="table table-sm">
+      <thead class="table-warning">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Description</th>
+          <th scope="col">Price</th>
+          <th scope="col">HCopy</th>
+          <th scope="col">Qty Returned</th>
+          <th scope="col">Unit</th>
+          <th scope="col">Qty Inspect</th>
+          <th scope="col">Date</th>
+          <th scope="col">Good Condition</th>
+          <th scope="col">Minor Repair/Clean</th>
+          <th scope="col">Repair/Parts Needed</th>
+          <th scope="col">Damaged</th>
+          <th scope="col">Comments</th>
+          <th scope="col">Doc #</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <header-line
+          v-for="(line, index) in lines"
+          :key="line.id"
+          :line-number="index + 1"
+          :line-details="line"
+          :header-is-locked="headerIsLocked"
+          @delete-line="getCpoLines"
+        ></header-line>
+      </tbody>
+    </table>
 
-        <div class="btn-group btn-group-sm" role="group" v-if="!headerIsLocked">
-            <button
-                v-if="!isInsertingNewLine"
-                class="btn btn-primary"
-                @click="addNewLine"
-            >
-                Add new line
-            </button>
-            <button class="btn btn-primary" type="button" disabled v-else>
-                Add new line
-                <span
-                    class="spinner-border spinner-border-sm"
-                    role="status"
-                    aria-hidden="true"
-                ></span>
-            </button>
-            <button
-                class="btn btn-success"
-                @click="saveAllLines"
-                v-if="!isSavingAllLines"
-            >
-                Save All lines
-            </button>
-            <button class="btn btn-success" type="button" disabled v-else>
-                Save All lines
-                <span
-                    class="spinner-border spinner-border-sm"
-                    role="status"
-                    aria-hidden="true"
-                ></span>
-            </button>
-        </div>
+    <div class="btn-group btn-group-sm" role="group" v-if="!headerIsLocked">
+      <button
+        v-if="!isInsertingNewLine"
+        class="btn btn-primary"
+        @click="addNewLine"
+      >
+        Add new line
+      </button>
+      <button class="btn btn-primary" type="button" disabled v-else>
+        Add new line
+        <span
+          class="spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true"
+        ></span>
+      </button>
+      <button
+        class="btn btn-success"
+        @click="saveAllLines"
+        v-if="!isSavingAllLines"
+      >
+        Save All lines
+      </button>
+      <button class="btn btn-success" type="button" disabled v-else>
+        Save All lines
+        <span
+          class="spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true"
+        ></span>
+      </button>
     </div>
+  </div>
 </template>
 
 <script>
 import headerLine from "./headerLine.vue";
 export default {
-    components: {
-        headerLine,
-    },
-    props: ["headerId", "headerIsLocked"],
-    data() {
-        return {
-            lines: [],
-            isInsertingNewLine: false,
-            isSavingAllLines: false,
-        };
-    },
+  components: {
+    headerLine,
+  },
+  props: ["headerId", "headerIsLocked"],
+  data() {
+    return {
+      lines: [],
+      isInsertingNewLine: false,
+      isSavingAllLines: false,
+    };
+  },
 
-    methods: {
-        addNewLine() {
-            this.isInsertingNewLine = true;
-            axios
-                .post("api/cpoline/", { id: this.headerId })
-                .then((res) => {
-                    this.getCpoLines();
-                    this.isInsertingNewLine = false;
-                    // console.log(this.lineDetails);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        },
-        saveAllLines() {
-            this.isSavingAllLines = true;
+  methods: {
+    async addNewLine() {
+      this.isInsertingNewLine = true;
+      try {
+        const res = await axios.post("api/cpoline/", { id: this.headerId });
 
-            axios
-                .post("api/cpo/lines/updateAllLines/", {
-                    cpoId: this.headerId,
-                    cpoLines: this.lines,
-                })
-                .then((res) => {
-                    this.isSavingAllLines = false;
-                    //this.getCpoLines();
-                    // this.isUpdatedAllLinesSuccess = true;
-                    // console.log("save all", res);
-                    // this.getCpoHeaderRow();
-                    // this.$emit("updated-header-lines");
-                    // setTimeout(() => {
-                    //     this.isUpdatedAllLinesSuccess = false;
-                    // }, 3000);
-                    // console.log(res.data);
-                    // console.log("saved all");
-                })
-                .catch((err) => {
-                    // console.log(err);
-                });
-        },
-        getCpoLines() {
-            axios
-                .get("api/cpo/lines/" + this.headerId)
-                .then((response) => {
-                    this.lines = response.data.lines;
-                    //console.log(this.lines);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        },
-    },
-    mounted() {
         this.getCpoLines();
-        // console.log("headerId: " + this.headerId);
+
+        // console.log(this.lineDetails);
+      } catch (error) {
+        console.log(err);
+      } finally {
+        this.isInsertingNewLine = false;
+      }
     },
+    async saveAllLines() {
+      this.isSavingAllLines = true;
+      try {
+        await axios.post("api/cpo/lines/updateAllLines/", {
+          cpoId: this.headerId,
+          cpoLines: this.lines,
+        });
+
+        this.isSavingAllLines = false;
+        //this.getCpoLines();
+        // this.isUpdatedAllLinesSuccess = true;
+        // console.log("save all", res);
+        // this.getCpoHeaderRow();
+        // this.$emit("updated-header-lines");
+        // setTimeout(() => {
+        //     this.isUpdatedAllLinesSuccess = false;
+        // }, 3000);
+        // console.log(res.data);
+        // console.log("saved all");
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        this.isSavingAllLines = false;
+      }
+    },
+    async getCpoLines() {
+      try {
+        const res = await axios.get("api/cpo/lines/" + this.headerId);
+
+        this.lines = res.data.lines;
+        //console.log(this.lines);
+      } catch (error) {
+        console.log(error.message);
+        // alert(error.message);
+      }
+    },
+  },
+  mounted() {
+    this.getCpoLines();
+    // console.log("headerId: " + this.headerId);
+  },
 };
 </script>
 
