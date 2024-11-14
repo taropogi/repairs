4ad3fdd585034b8f827984f078da3edfd55042df
Richard Bernisca <taropogi_123@yahoo.com>
2024-11-14 14:13:56 +0800

    display permissions

--- resources/js/apps/cpo/components/admin/UserPage/UserItem.vue
+++ resources/js/apps/cpo/components/admin/UserPage/UserItem.vue
@@ -1,51 +1,76 @@
 <template>
-    <tr>
-        <th scope="row">
-            {{ user.username }}
-        </th>
-        <td>{{ user.name }}</td>
-        <td>{{ user.email }}</td>
-        <td>{{ isAdmin }}</td>
-        <td>
-            <div class="btn-group btn-group-sm" role="group">
-                <router-link class="btn btn-primary" :to="editUserLink">
-                    EDIT
-                </router-link>
+  <tr>
+    <th scope="row">
+      {{ user.username }}
+    </th>
+    <td>{{ user.name }}</td>
+    <td>{{ user.email }}</td>
+    <td>
+      <p v-if="isAdmin" class="badge bg-primary me-1">Admin</p>
+      <div v-else>
+        <span
+          v-for="(permission, index) in permissions"
+          :key="permission"
+          class="m-0 p-0"
+        >
+          {{ formatPermission(permission) }}
+          <span v-if="index < permissions.length - 1"> | </span>
+        </span>
+      </div>
+    </td>
+    <td>
+      <div class="btn-group btn-group-sm" role="group">
+        <router-link class="btn btn-primary" :to="editUserLink">
+          EDIT
+        </router-link>
 
-                <button
-                    type="button"
-                    class="btn btn-danger"
-                    @click="deleteUser"
-                >
-                    DELETE
-                </button>
-            </div>
-        </td>
-    </tr>
+        <button type="button" class="btn btn-danger" @click="deleteUser">
+          DELETE
+        </button>
+      </div>
+    </td>
+  </tr>
 </template>
 
 <script>
 export default {
-    props: ["user"],
-    emits: ["delete-user"],
-    computed: {
-        isAdmin() {
-            return this.user.is_admin ? "Yes" : "";
-        },
-        editUserLink() {
-            return {
-                name: "admin-user-edit",
-                params: {
-                    id: this.user.id,
-                },
-            };
-        },
+  props: ["user"],
+  emits: ["delete-user"],
+  computed: {
+    permissions() {
+      if (this.user.is_admin) {
+        return "Admin";
+      }
+
+      if (!this.user.permissions) {
+        return [];
+      }
+
+      return this.user.permissions;
     },
-    methods: {
-        deleteUser() {
-            this.$emit("delete-user", this.user);
-        },
+    isAdmin() {
+      return this.user.is_admin ? "Yes" : "";
     },
+    editUserLink() {
+      return {
+        name: "admin-user-edit",
+        params: {
+          id: this.user.id,
+        },
+      };
+    },
+  },
+  methods: {
+    deleteUser() {
+      this.$emit("delete-user", this.user);
+    },
+    formatPermission(permission) {
+      return permission
+        .split("-")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+    },
+  },
 };
 </script>
 
--- resources/js/apps/cpo/components/admin/UserPage/UserList.vue
+++ resources/js/apps/cpo/components/admin/UserPage/UserList.vue
@@ -1,81 +1,81 @@
 <template>
-    <div class="p-5">
-        <modal-delete-user
-            v-if="deleteUser"
-            :user="deleteUser"
-            @close="deleteUser = null"
-            @deleted-user="getUserList"
-        ></modal-delete-user>
-        <table class="table table-striped" v-if="userList">
-            <thead>
-                <tr class="table-primary">
-                    <th scope="col">Username</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Admin</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <user-item
-                    v-for="user in userList"
-                    :key="user.id"
-                    :user="user"
-                    @delete-user="openDeleteUserModal"
-                ></user-item>
-            </tbody>
-        </table>
-    </div>
+  <div class="p-5">
+    <modal-delete-user
+      v-if="deleteUser"
+      :user="deleteUser"
+      @close="deleteUser = null"
+      @deleted-user="getUserList"
+    ></modal-delete-user>
+    <table class="table table-striped" v-if="userList">
+      <thead>
+        <tr class="table-primary">
+          <th scope="col">Username</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Permissions</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <user-item
+          v-for="user in userList"
+          :key="user.id"
+          :user="user"
+          @delete-user="openDeleteUserModal"
+        ></user-item>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 import UserItem from "./UserItem.vue";
 import ModalDeleteUser from "./ModalDeleteUser.vue";
 export default {
-    components: {
-        UserItem,
-        ModalDeleteUser,
-    },
-    props: ["searchCriteria"],
-    data() {
-        return {
-            deleteUser: null,
-            userList: null,
-        };
-    },
+  components: {
+    UserItem,
+    ModalDeleteUser,
+  },
+  props: ["searchCriteria"],
+  data() {
+    return {
+      deleteUser: null,
+      userList: null,
+    };
+  },
 
-    watch: {
-        searchCriteria: {
-            handler(newVal, oldVal) {
-                this.getUserList();
-            },
-            deep: true,
-        },
-    },
-    methods: {
-        openDeleteUserModal(user) {
-            this.deleteUser = user;
-        },
-        async getUserList() {
-            this.deleteUser = null;
-            await axios
-                .get("/api/users/list", {
-                    params: {
-                        ...this.searchCriteria,
-                    },
-                })
-                .then((response) => {
-                    this.userList = response.data.users;
-                    // console.log(response.data);
-                })
-                .catch((error) => {
-                    // console.log(error);
-                });
-        },
-    },
-    mounted() {
+  watch: {
+    searchCriteria: {
+      handler(newVal, oldVal) {
         this.getUserList();
+      },
+      deep: true,
     },
+  },
+  methods: {
+    openDeleteUserModal(user) {
+      this.deleteUser = user;
+    },
+    async getUserList() {
+      this.deleteUser = null;
+      await axios
+        .get("/api/users/list", {
+          params: {
+            ...this.searchCriteria,
+          },
+        })
+        .then((response) => {
+          this.userList = response.data.users;
+          // console.log(response.data);
+        })
+        .catch((error) => {
+          // console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.getUserList();
+  },
 };
 </script>
 
