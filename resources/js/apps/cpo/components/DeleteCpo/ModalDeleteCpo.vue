ecec2e32e64072e0bd648a5a66c9dbaa8aceb57b
Richard Bernisca <taropogi_123@yahoo.com>
2024-11-29 14:56:32 +0800

    delete async

--- resources/js/apps/cpo/components/DeleteCpo/ModalDeleteCpo.vue
+++ resources/js/apps/cpo/components/DeleteCpo/ModalDeleteCpo.vue
@@ -1,97 +1,109 @@
 <template>
-    <base-modal>
-        <template #header>
-            <h5 class="modal-title text-white">
-                <strong>Are you sure to delete this CPO?</strong>
-            </h5>
-            <button
-                type="button"
-                class="btn-close"
-                @click="$emit('close-modal-delete-cpo')"
-            ></button>
-        </template>
-        <template #body v-if="headerRow">
-            <table class="table table-sm">
-                <thead>
-                    <tr class="table-primary">
-                        <th scope="col">CPO#</th>
-                        <th scope="col">Customer</th>
-                        <th scope="col">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">{{ headerRow.id }}</th>
-                        <td>{{ headerRow.customer_name }}</td>
-                        <td>{{ headerRow.status.status }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </template>
-        <template #default v-if="!isDeleting">
-            <button type="button" class="btn btn-danger" @click="confirmDelete">
-                Yes
-            </button>
-            <button
-                type="button"
-                class="btn btn-primary"
-                @click="$emit('close-modal-delete-cpo')"
-            >
-                No
-            </button>
-        </template>
-    </base-modal>
+  <base-modal>
+    <template #header>
+      <h5 class="modal-title text-white">
+        <strong> Are you sure you want to delete this CPO?</strong>
+      </h5>
+      <button
+        type="button"
+        class="btn-close"
+        @click="$emit('close-modal-delete-cpo')"
+      ></button>
+    </template>
+    <template #body v-if="headerRow">
+      <table class="table table-sm">
+        <thead>
+          <tr class="table-primary">
+            <th scope="col">CPO#</th>
+            <th scope="col">Customer</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">{{ headerRow.id }}</th>
+            <td>{{ headerRow.customer_name }}</td>
+            <td>{{ headerRow.status.status }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </template>
+    <template #default v-if="!isDeleting">
+      <button type="button" class="btn btn-danger" @click="confirmDelete">
+        Yes
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary"
+        @click="$emit('close-modal-delete-cpo')"
+      >
+        No
+      </button>
+    </template>
+  </base-modal>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
 export default {
-    props: ["cpoId"],
+  props: ["cpoId"],
 
-    emits: ["deleted-cpo", "close-modal-delete-cpo"],
-    data() {
-        return {
-            headerRow: null,
-            isDeleting: false,
-        };
-    },
+  emits: ["deleted-cpo", "close-modal-delete-cpo"],
+  data() {
+    return {
+      headerRow: null,
+      isDeleting: false,
+    };
+  },
 
-    methods: {
-        ...mapActions("cpo", ["removeSelectedPo", "addDeletedCpos"]),
-        unselectPo() {
-            this.removeSelectedPo({
-                id: this.cpoId,
-            });
-        },
-        async confirmDelete() {
-            this.isDeleting = true;
-            const res = await axios.post("api/cpo/destroy", {
-                cpoId: this.cpoId,
-            });
-            if (res.data) {
-                this.unselectPo();
-                // this.addDeletedCpos({
-                //     id: this.cpoId,
-                // });
-                // this.isDeleted = true;
+  methods: {
+    ...mapActions("cpo", ["removeSelectedPo", "addDeletedCpos"]),
+    unselectPo() {
+      this.removeSelectedPo({
+        id: this.cpoId,
+      });
+    },
+    async confirmDelete() {
+      this.isDeleting = true;
+      try {
+        const res = await axios.post("api/cpo/destroy", {
+          cpoId: this.cpoId,
+        });
+        if (res.data) {
+          this.unselectPo();
+          // this.addDeletedCpos({
+          //     id: this.cpoId,
+          // });
+          // this.isDeleted = true;
 
-                this.$emit("deleted-cpo", { id: this.cpoId });
-            }
-        },
-        getCpoHeader() {
-            axios
-                .get("api/cpo/" + this.cpoId)
-                .then((response) => {
-                    this.headerRow = response.data.cpo;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        },
+          this.$emit("deleted-cpo", { id: this.cpoId });
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isDeleting = false;
+      }
     },
-    mounted() {
-        this.getCpoHeader();
+    async getCpoHeader() {
+      try {
+        const res = await axios.get("api/cpo/" + this.cpoId);
+        this.headerRow = res.data.cpo;
+      } catch (error) {
+        console.log(error);
+      }
+      //   axios
+      //     .get("api/cpo/" + this.cpoId)
+      //     .then((response) => {
+      //       this.headerRow = response.data.cpo;
+      //     })
+      //     .catch((error) => {
+      //       console.log(error);
+      //     });
     },
+  },
+  mounted() {
+    this.getCpoHeader();
+  },
 };
 </script>
 
