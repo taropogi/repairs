76465f17ba728dbcc5344559ae3ef0f4413d348a
Richard Bernisca <taropogi_123@yahoo.com>
2024-12-06 11:57:58 +0800

    fix encode line error

--- app/Http/Controllers/CpoController.php
+++ app/Http/Controllers/CpoController.php
@@ -80,16 +80,19 @@ class CpoController extends Controller
             $line = new CpoLine();
             $line->line_number = $item['lineNumber'];
             $line->description = $item['description'] ?? null;
-            $line->price = $item['price'] ?? null;
+
+            $line->price = isset($item['price']) && is_numeric($item['price']) ? $item['price'] : null;
             $line->hcopy = $item['hcopy'] ?? null;
-            $line->qty_returned = $item['qtyReturned'] ?? null;
+
+            $line->qty_returned = isset($item['qtyReturned']) && is_numeric($item['qtyReturned']) ? $item['qtyReturned'] : null;
+
             $line->unit = $item['unit'] ?? null;
-            $line->qty_inspect = $item['qtyInspect'] ?? null;
+            $line->qty_inspect = isset($item['qtyInspect']) && is_numeric($item['qtyInspect']) ? $item['qtyInspect'] : null;
             $line->date = $item['date'] ?? null;
-            $line->good_condition = $item['goodCondition'] ?? null;
-            $line->minor_repair_clean = $item['minorRepairClean'] ?? null;
-            $line->repair_parts_needed = $item['repairPartsNeeded'] ?? null;
-            $line->damaged = $item['damaged'] ?? null;
+            $line->good_condition = isset($item['goodCondition']) && is_numeric($item['goodCondition']) ? $item['goodCondition'] : null;
+            $line->minor_repair_clean = isset($item['minorRepairClean']) && is_numeric($item['minorRepairClean']) ? $item['minorRepairClean'] : null;
+            $line->repair_parts_needed = isset($item['repairPartsNeeded']) && is_numeric($item['repairPartsNeeded']) ? $item['repairPartsNeeded'] : null;
+            $line->damaged = isset($item['damaged']) && is_numeric($item['damaged']) ? $item['damaged'] : null;
             $line->comments = $item['comments'] ?? null;
             $line->order_number = $item['orderNumber'] ?? null;
             $line->cpo_id = $new_cpo->id;
