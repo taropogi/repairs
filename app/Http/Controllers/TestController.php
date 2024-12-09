086aed2e593d595596a3bb432ec30b4b819e459e
Richard Bernisca <taropogi_123@yahoo.com>
2024-12-09 16:30:37 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -31,7 +31,7 @@ class TestController extends Controller
             echo '<h1>No image</h1>';
         }
 
-        $this->getItemsAll();
+        // $this->getItemsAll();
 
         // $url = "http://113.1.1.190/phase7/pictures/003438.jpg";
 
