a19af437c75edd5be0fdf1221dcdefb58a79a34b
Richard Bernisca <taropogi_123@yahoo.com>
2024-12-09 16:34:14 +0800

    headers

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -24,7 +24,7 @@ class TestController extends Controller
 
         $url = "http://113.1.1.190/phase7/pictures/003438.jpg";
         $headers = @get_headers($url);
-
+        echo $headers;
         if ($headers && strpos($headers[0], '200') !== false) {
             echo '<h1>has image</h1>';
         } else {
