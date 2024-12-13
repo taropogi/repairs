823e7ca499a2a425ee1c92a46e05d08cbfa028a2
Richard Bernisca <taropogi_123@yahoo.com>
2024-12-13 16:54:42 +0800

    renmove test

--- resources/js/apps/cpo/App.vue
+++ resources/js/apps/cpo/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>test</h1>
     <spinner-loading v-if="!oracleCustomers"></spinner-loading>
     <main class="container-fluid" v-else>
       <div :class="{ backdrop: showBackDrop }"></div>
