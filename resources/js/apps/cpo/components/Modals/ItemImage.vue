2d2e36dc9056499acf6ac1690cada6ef2c5e32c5
Times Trading <timesgacc@gmail.com>
2024-12-18 16:50:32 +0800

    clikable image

--- resources/js/apps/cpo/components/Modals/ItemImage.vue
+++ resources/js/apps/cpo/components/Modals/ItemImage.vue
@@ -6,6 +6,7 @@
       class="item-image img-fluid mx-2"
       style="max-height: 50px;"
       @error="handleImageError"
+      @click="openImage"
     />
   </span>
 </template>
@@ -27,8 +28,26 @@ export default {
     handleImageError() {
       this.error = true;
     },
+    openImage() {
+    
+      const popupWidth = 600;
+      const popupHeight = 400;
+      const left = (screen.width / 2) - (popupWidth / 2);
+      const top = (screen.height / 2) - (popupHeight / 2);
+      window.open(this.url, '_blank', `width=${popupWidth},height=${popupHeight},top=${top},left=${left}`);
+      
+    
+    }
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.item-image {
+  cursor: pointer;
+}
+.item-image:hover {
+  border: 2px solid red;
+  box-shadow: 0 0 10px red;
+}
+</style>
