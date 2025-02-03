03793237d23ab685db2230ac7d4384d9ac49e156
Times Trading <timesgacc@gmail.com>
2025-02-03 10:25:50 +0800

    add activity log for save all lines

--- app/Http/Controllers/CpoController.php
+++ app/Http/Controllers/CpoController.php
@@ -303,6 +303,13 @@ class CpoController extends Controller
 
 
         $cpo->touch();
+
+        auth()->user()->activities()->create([
+            'action' => 'Update All Lines',
+            'description' => 'Updated all lines for CPO with ID:' . $cpo->id
+        ]);
+
+
         // set delay time sleep for 0.5 second
         usleep(500000);
         return $request;
