543bb467aa5e09c317677fa301152da8cc023c38
Times Trading <timesgacc@gmail.com>
2025-02-04 09:41:22 +0800

    admin footer

--- resources/js/apps/cpo/components/admin/AdminPage/AdminPage.vue
+++ resources/js/apps/cpo/components/admin/AdminPage/AdminPage.vue
@@ -1,20 +1,25 @@
 <template>
-    <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <side-bar></side-bar>
-        </div>
-        <div class="col p-1">
-            <router-view v-slot="slotProps">
-                <transition name="router" mode="out-in">
-                    <component :is="slotProps.Component"></component>
-                </transition>
-            </router-view>
-        </div>
+  <div class="row flex-nowrap">
+    <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+      <side-bar></side-bar>
     </div>
+    <div class="col p-1">
+      <router-view v-slot="slotProps">
+        <transition name="router" mode="out-in">
+          <component :is="slotProps.Component"></component>
+        </transition>
+      </router-view>
+      <page-footer></page-footer>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import PageFooter from "../../pages/pageFooter.vue";
+export default {
+  components: { PageFooter },
+};
 </script>
 
 <style></style>
+PageFooter
