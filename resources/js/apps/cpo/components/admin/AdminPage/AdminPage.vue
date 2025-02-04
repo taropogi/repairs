9080b8b9073dff33d8c7cbed9db149429eb1c1ee
Times Trading <timesgacc@gmail.com>
2025-02-04 10:21:43 +0800

    admin footer

--- resources/js/apps/cpo/components/admin/AdminPage/AdminPage.vue
+++ resources/js/apps/cpo/components/admin/AdminPage/AdminPage.vue
@@ -3,7 +3,12 @@
     <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
       <side-bar></side-bar>
     </div>
-    <div class="col p-1" style="margin-bottom: 200px">
+    <div
+      class="col p-1"
+      :class="{
+        'has-margin': !isNavSearchActive,
+      }"
+    >
       <router-view v-slot="slotProps">
         <transition name="router" mode="out-in">
           <component :is="slotProps.Component"></component>
@@ -16,10 +21,20 @@
 
 <script>
 import PageFooter from "../../pages/pageFooter.vue";
+import { mapGetters } from "vuex";
 export default {
   components: { PageFooter },
+  computed: {
+    ...mapGetters(["activeNav"]),
+    isNavSearchActive() {
+      return this.activeNav?.nav === "search-cpo" ? true : false;
+    },
+  },
 };
 </script>
 
-<style></style>
-PageFooter
+<style scoped>
+.has-margin {
+  margin-bottom: 200px;
+}
+</style> 
