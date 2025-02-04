486dcf70987b84d3ed75e0a7a83c34ecee932856
Times Trading <timesgacc@gmail.com>
2025-02-04 09:39:49 +0800

    user page footer

--- resources/js/apps/cpo/components/pages/cpoPage.vue
+++ resources/js/apps/cpo/components/pages/cpoPage.vue
@@ -1,45 +1,49 @@
 <template>
-    <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <side-bar></side-bar>
-        </div>
-        <div class="col p-1">
-            <router-view v-slot="slotProps">
-                <transition name="router" mode="out-in">
-                    <component :is="slotProps.Component"></component>
-                </transition>
-            </router-view>
-        </div>
+  <div class="row flex-nowrap">
+    <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+      <side-bar></side-bar>
     </div>
+    <div class="col p-1">
+      <router-view v-slot="slotProps">
+        <transition name="router" mode="out-in">
+          <component :is="slotProps.Component"></component>
+        </transition>
+      </router-view>
+      <page-footer></page-footer>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import pageFooter from "./pageFooter.vue";
+export default {
+  components: { pageFooter },
+};
 </script>
 
 <style>
 .router-enter-from {
-    opacity: 0;
+  opacity: 0;
 
-    transform: translateY(-30px);
+  transform: translateY(-30px);
 }
 .router-enter-active {
-    transition: all 0.3s ease-out;
+  transition: all 0.3s ease-out;
 }
 .router-enter-to {
-    opacity: 1;
+  opacity: 1;
 
-    transform: translateY(0);
+  transform: translateY(0);
 }
 .router-leave-from {
-    opacity: 1;
-    transform: translateY(0);
+  opacity: 1;
+  transform: translateY(0);
 }
 .router-leave-active {
-    transition: all 0.3s ease-out;
+  transition: all 0.3s ease-out;
 }
 .router-leave-to {
-    opacity: 0;
-    transform: translateY(30px);
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
---
+++ resources/js/apps/cpo/components/pages/pageFooter.vue
@@ -0,0 +1,51 @@
+<template>
+  <footer
+    class="py-0 bg-dark text-white mt-auto footer"
+    style="position: fixed; bottom: 0; width: 100%"
+  >
+    <blockquote class="blockquote p-0 mb-0 mx-4 d-flex align-items-center">
+      <p class="mb-0 d-inline quote-text">"{{ quote }}"</p>
+      <footer class="blockquote-footer my-0 ms-2">
+        {{ author }}
+      </footer>
+    </blockquote>
+  </footer>
+</template>
+
+<script>
+import { QUOTES } from "../../utils/constArray.js";
+export default {
+  data() {
+    return {
+      quote: "",
+      author: "",
+    };
+  },
+  methods: {
+    async fetchQuote() {
+      const randomIndex = Math.floor(Math.random() * QUOTES.length);
+      const randomQuote = QUOTES[randomIndex];
+      this.quote = randomQuote.text;
+      this.author = randomQuote.author;
+    },
+  },
+  mounted() {
+    this.fetchQuote();
+    setInterval(() => {
+      this.fetchQuote();
+    }, 30000);
+  },
+};
+</script>
+
+<style scoped>
+.footer {
+  display: flex;
+  height: 30px; /* Adjust height as needed */
+}
+
+.blockquote-footer {
+  font-size: 0.9em; /* Adjust font size as needed */
+  color: #ccc; /* Adjust color as needed */
+}
+</style>
